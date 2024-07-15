498361f701fecb016ed86a647732204529220768
Sonu Sahu <114206751+sonusahuz@users.noreply.github.com>
2024-07-15 16:12:28 +0530

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -21,7 +21,7 @@ export const project = [
     description:
       'Introducing Musicolet, a dynamic music website powered by Next.js 14, TypeScript, and TailwindCSS. Leveraging the JioSaavn API via Context API, Musicolet offers seamless access to a vast library of music tracks and albums. trending, albums, top charts, and enjoy personalized playlists and recommendations, all with a modern and responsive user interface.',
     link: 'https://melodify-music.vercel.app',
-    github: 'https://github.com/sonusahuz/melodify',
+    github: 'https://github.com/sonusahuz/melodify-music',
   },
 ];
 
